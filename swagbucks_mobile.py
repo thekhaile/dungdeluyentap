ba3dd05862fc52c0994f2fba60b4e0305ce52649
Khai Le <khai.le@mutualmobile.com>
2016-10-11 22:41:29 -0500

    added loops

--- swagbucks_mobile.py
+++ swagbucks_mobile.py
@@ -55,22 +55,32 @@ def main():
     #get Listview
     listView = app.find_element(MobileBy.CLASS_NAME, 'android.widget.ListView')
     items = listView.find_elements(MobileBy.CLASS_NAME, 'android.widget.RelativeLayout')
-    item = items[5]
-    UIType.Button(item).tap()
 
-    View = app.find_element(MobileBy.CLASS_NAME, 'android.support.v7.widget.RecyclerView')
-    cell = View.find_element(MobileBy.CLASS_NAME, 'android.widget.FrameLayout')
-    coordinates = cell.location
-    size = cell.size
-    coordinates['x'] = coordinates['x']+ size['width']/2
-    coordinates['y'] = coordinates['y']+ size['height']/2
-    action.long_press(x=coordinates['x'], y=coordinates['y']).perform()
-    sleep(4)
+    for item in items[5:15]:
+        UIType.Button(item).tap()
+        for i in range(50):
+            View = app.find_element(MobileBy.CLASS_NAME, 'android.support.v7.widget.RecyclerView')
+            cells = View.find_elements(MobileBy.CLASS_NAME, 'android.widget.FrameLayout')
+            for cell in cells:
+                coordinates = cell.location
+                size = cell.size
+                coordinates['x'] = coordinates['x']+ size['width']/2
+                coordinates['y'] = coordinates['y']+ size['height']/2
+                action.long_press(x=coordinates['x'], y=coordinates['y']).perform()
+                sleep(4)
+                #Add to favorites
+                if app.find_element(MobileBy.ANDROID_UIAUTOMATOR, 'text("Add to Favorites")'):
+                    el = app.find_element(MobileBy.ANDROID_UIAUTOMATOR, 'text("Add to Favorites")')
+                    UIType.Button(el).tap()
+                else:
+                    el = app.find_element(MobileBy.ANDROID_UIAUTOMATOR, 'text("Cancel")')
+                    UIType.Button(el).tap()
+            app.swipe_up()
 
-
-    #Add to favorites
-    el = app.find_element(MobileBy.ANDROID_UIAUTOMATOR, 'text("Add to Favorites")')
-    UIType.Button(el).tap()
+            #Tap menu icon
+        el = app.find_element(MobileBy.ID, 'Open navigation drawer')
+        UIType.Button(el).tap()
+        items = listView.find_elements(MobileBy.CLASS_NAME, 'android.widget.RelativeLayout')
 
     #Tap menu icon
     el = app.find_element(MobileBy.ID, 'Open navigation drawer')
